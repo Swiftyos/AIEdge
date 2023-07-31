0b214c11d67c25966d180c887dea8703d4b21b2a
SwiftyOS <craigswift13@gmail.com>
2023-07-31 14:30:53 +0200

    Changed header

--- components/header.tsx
+++ components/header.tsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Blog
+        AI Edge
       </Link>
       .
     </h2>
   )
-}
+}s
 
 export default Header
